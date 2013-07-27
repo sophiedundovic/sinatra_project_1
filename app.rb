e4aac65fe9b124f8c706f21bc3475d1abb8534cf
Sophie Dundovic <sophie.dundovic@gmail.com>
2013-07-27 18:34:08 +0100

    added app content

--- app.rb
+++ app.rb
@@ -2,4 +2,23 @@ require 'sinatra'
 
 get '/' do
     erb :index
+
 end
+
+get '/:name1' do
+    @name1 = params[:name1]
+    erb :greet2
+end
+
+
+get '/:name' do
+    "Hello #{params[:name]}"
+end
+
+get '/:name1/:name2/:adjective' do
+    "#{params[:name1]} #{params[:name2]} #{params[:adjective}"
+end
+
+
+        
+        # my app. to do - name 2 and adjective. 
