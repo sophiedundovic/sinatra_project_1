3c145a1564f4dca8d908ada9272950041a20e476
Sophie Dundovic <sophie.dundovic@gmail.com>
2013-07-27 18:56:12 +0100

    added erb greet2

--- app.rb
+++ app.rb
@@ -5,18 +5,11 @@ get '/' do
 
 end
 
-get '/:name1' do
-    @name1 = params[:name1]
-    erb :greet2
-end
-
-
-get '/:name' do
-    "Hello #{params[:name]}"
-end
 
 get '/:name1/:name2/:adjective' do
-    "#{params[:name1]} #{params[:name2]} #{params[:adjective]}"
+    @name1 = params[:name1]
+    @name2 = params[:name2]
+    @adjective = params[:adjective]
     erb :greet2 
 end
 
